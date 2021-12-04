fdeeba1d9257d88e05d4d40c4fced508f56d8797
ZingyTomato <79736973+ZingyTomato@users.noreply.github.com>
2021-12-04 19:49:54 +0530

    Various Changes
    Added support for youtube thumbnails, video length, cleaned up many embeds and added labels for each command

--- bot.py
+++ bot.py
@@ -5,10 +5,12 @@ import nextcord
 from nextcord.ext import commands
 import json
 import lyricsgenius
-
-
+import os 
+import subprocess
+import shlex
 
 # Suppress noise about console usage from errors
+
 youtube_dl.utils.bug_reports_message = lambda: ''
 
 
@@ -32,6 +34,7 @@ ffmpeg_options = {
 
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
+# Get audio using youtube-dl
 
 class YTDLSource(nextcord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -54,59 +57,66 @@ class YTDLSource(nextcord.PCMVolumeTransformer):
         filename = data['url'] if stream else ytdl.prepare_filename(data)
         return cls(nextcord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
+# Main music cog
+
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.command()
     async def stream(self, ctx, *, url):
-      player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-      ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
-      embed=nextcord.Embed(title="Found the song!", description=f"Now playing ```{player.title}```", color = nextcord.Color.green())
-      await ctx.reply(embed=embed,mention_author=False)
+      async with ctx.typing():
+      	player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+      	ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+      	cmd=shlex.split(f'youtube-dl "ytsearch:{url}" --get-thumbnail')
+      	cmd2=shlex.split(f'youtube-dl "ytsearch:{url}" --get-duration')
+      	img = subprocess.check_output(cmd)
+      	img2 = subprocess.check_output(cmd2)
+      	finalimg = img.decode('utf-8')
+      	finaldur = img2.decode('utf-8')
+      	embed=nextcord.Embed(description=f"**Currently Playing!**", color = nextcord.Color.green())
+      	embed.add_field(name = "Title", value =f"{player.title}", inline = False)
+      	embed.add_field(name = "Length", value =f"{finaldur}", inline = False)
+      	embed.set_image(url=finalimg)
+      	await ctx.reply(embed=embed,mention_author=False)
     @commands.command()
     async def pause(self, ctx):
         song = ctx.voice_client.pause()
-        embed=nextcord.Embed(title="Song paused!", description=f"Paused!", color = nextcord.Color.red())
+        embed=nextcord.Embed(description=f"I've **paused** the currently playing song!", color = nextcord.Color.red())
         await ctx.reply(embed=embed, mention_author=False)
     @commands.command()
     async def resume(self, ctx):
         song = ctx.voice_client.resume()
-        embed=nextcord.Embed(title="Song resumed!", description=f"Resumed!", color = nextcord.Color.green())
+        embed=nextcord.Embed(description=f"Resuming...", color = nextcord.Color.green())
         await ctx.reply(embed=embed, mention_author=False)
     @commands.command()
     async def leave(self, ctx):
         voicetrue = ctx.author.voice
         myvoicetrue = ctx.guild.me.voice
         if voicetrue is None:
-            embed=nextcord.Embed(title="Voice channel not found", description="You have not joined a voice channel!", color = nextcord.Color.red())
+            embed=nextcord.Embed(description="You have not **joined** a voice channel!", color = nextcord.Color.red())
             return await ctx.reply(embed=embed, mention_author=False)
         if myvoicetrue  is None:
-            embed=nextcord.Embed(title="Voice channel not found", description="I am not currently in a voice channel!", color = nextcord.Color.red())
+            embed=nextcord.Embed(description="I am not currently **in** a voice channel!", color = nextcord.Color.red())
             return await ctx.reply(embed=embed, mention_author=False)
         await ctx.voice_client.disconnect()
-        embed=nextcord.Embed(title="Leaving VC!", description=f"I have left your voice channel!", color = nextcord.Color.green())
+        embed=nextcord.Embed(description=f"I have **left** your voice channel!", color = nextcord.Color.green())
         await ctx.reply(embed=embed, mention_author=False)
     @commands.command()
     async def stop(self, ctx):
         song = ctx.voice_client.stop()
-        embed=nextcord.Embed(title="Stopped", description=f"Song stopped!", color = nextcord.Color.red())
+        embed=nextcord.Embed(description=f"I've **stopped** the song!", color = nextcord.Color.red())
         await ctx.reply(embed=embed, mention_author=False)
     @commands.command()
     async def join(self, ctx):
         if ctx.voice_client is None:
             if ctx.author.voice:
                 await ctx.author.voice.channel.connect()
-                embed=nextcord.Embed(title="Joined Voice Channel!", description=f"I am now in your voice channel!", color = nextcord.Color.green())
+                embed=nextcord.Embed(description=f"I am now **in** your voice channel!", color = nextcord.Color.green())
        	        await ctx.reply(embed=embed, mention_author=False)
             else:
-                embed=nextcord.Embed(title="Voice channel not found!", description=f"You are not in a voice channel! Kindly join one.", color = nextcord.Color.red())
+                embed=nextcord.Embed(description=f"You are not **in** a voice channel! Kindly join one.", color = nextcord.Color.red())
                 await ctx.reply(embed=embed, mention_author=False)
-    @commands.command()
-    async def np(self, ctx):
-      #player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-      embed=nextcord.Embed(title="Now playing!", description=f"Currently playing ```{player.title}```", color = nextcord.Color.green())
-      await ctx.reply(embed=embed, view=view, mention_author=False)
-
+                
     @stream.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
@@ -118,7 +128,11 @@ class Music(commands.Cog):
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
 
+# Set bot prefix
+
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("niko "), help_command=None)
+
+# Change bot status
 
 @bot.event
 async def on_ready():
@@ -143,6 +157,7 @@ class HelpDropdown(nextcord.ui.Select):
             embed.add_field(name = "niko resume", value = "Resumes the currently playing song/audio.")
             embed.add_field(name = "niko stop", value = "Stops the currently playing song/audio.")
             embed.add_field(name = "niko lyrics", value = "Find lyrics on your desired songs!")
+            embed.add_field(name = "niko invite", value = "Invite me to other servers!")
             return await interaction.response.edit_message(embed=embed)
 
 class DropdownView(nextcord.ui.View):
@@ -164,18 +179,29 @@ async def help(ctx):
     view = DropdownView()
     view.message = await ctx.reply(embed=embed, view=view, mention_author=False)
     
+# Lyrics command.
+ 
 @bot.command()
 @commands.cooldown(5, 30, commands.BucketType.user)
 async def lyrics(ctx, *, title):
-    genius = lyricsgenius.Genius("GENIUSTOKEN")
+    genius = lyricsgenius.Genius("TOKEN")
     genius.verbose = False
     genius.remove_section_headers = True
     genius.skip_non_songs = True
     song = genius.search_song(f"{title}")
-    #print(song.lyrics)
     embed=nextcord.Embed(title=f"Lyrics for {title}!" ,description=f"{song.lyrics}")
     await ctx.reply(embed=embed, mention_author=False)
-    
+  
+# Invite command 
+ 
+@bot.command()
+@commands.cooldown(5, 30, commands.BucketType.user)
+async def invite(ctx):
+	yes = nextcord.ui.View()
+	yes.add_item(nextcord.ui.Button(label="Invite me!", url="https://discord.com/api/oauth2/authorize?client_id=915595163286532167&permissions=66087744&scope=bot"))
+	embed=nextcord.Embed(description="Here's my invite link!")
+	await ctx.reply(embed=embed,mention_author=False, view=yes)
+
 # Error Handling
 
 @lyrics.error
@@ -185,4 +211,4 @@ async def on_command_error(ctx, error):
         await ctx.reply(embed=embed, mention_author=False)
 
 bot.add_cog(Music(bot))
-bot.run('BOT TOKEN')
+bot.run('TOKEN')
