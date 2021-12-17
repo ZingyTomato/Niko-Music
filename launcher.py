627d8c9df0dbdb9a36334cfb2cb4f006cc0dd78f
ZingyTomato <79736973+ZingyTomato@users.noreply.github.com>
2021-12-17 23:01:58 +0530

    Add slash commands library

--- launcher.py
+++ launcher.py
@@ -1,10 +1,11 @@
 from bot import MusicBot
 from discord_components import DiscordComponents, ComponentsBot, Button, Select, SelectOption
-
+from discord_slash import cog_ext, SlashContext, SlashCommand
 
 def main():
     bot = MusicBot()
     DiscordComponents(bot)
+    slash = SlashCommand(bot, sync_commands=True)
     bot.run()
 
 if __name__ == "__main__":
