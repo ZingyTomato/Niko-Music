2a1bd14bc5026211d4deb1c281bc9635580f2564
ZingyTomato <79736973+ZingyTomato@users.noreply.github.com>
2021-12-24 20:52:35 +0530

    Remove more undeeded libraries

--- launcher.py
+++ launcher.py
@@ -1,11 +1,7 @@
-from bot import MusicBot
-from discord_components import DiscordComponents, ComponentsBot, Button, Select, SelectOption
-from discord_slash import cog_ext, SlashContext, SlashCommand
+from bot import MusicBot	
 
 def main():
     bot = MusicBot()
-    DiscordComponents(bot)
-    slash = SlashCommand(bot, sync_commands=True)
     bot.run()
 
 if __name__ == "__main__":
