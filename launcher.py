b06b8a235577789b9964221c0b00eece5b0d476b
ZingyTomato <79736973+ZingyTomato@users.noreply.github.com>
2021-12-26 20:12:43 +0530

    Add libraries

--- launcher.py
+++ launcher.py
@@ -1,7 +1,9 @@
 from bot import MusicBot	
+from discord_components import DiscordComponents, ComponentsBot, Button
 
 def main():
     bot = MusicBot()
+    DiscordComponents(bot)
     bot.run()
 
 if __name__ == "__main__":
