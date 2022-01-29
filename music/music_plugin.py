61c1366b1782316b4a951f92a1becc0e14d0bcd4
Zingy Tomato <79736973+ZingyTomato@users.noreply.github.com>
2022-01-29 09:07:21 +0530

    Update imports

--- music/music_plugin.py
+++ music/music_plugin.py
@@ -13,6 +13,7 @@ from lightbulb.ext import neon
 import requests
 import json
 import os
+from datetime import date
 
 HIKARI_VOICE = False
 URL_REGEX = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
